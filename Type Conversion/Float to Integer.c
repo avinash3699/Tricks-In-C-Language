65fe1985497943345313f0d646814e5851cbf1b9
Avinash M <avinashmohan2001@gmail.com>
2021-06-13 10:44:16 +0530

    Type Conversion

---
+++ Type Conversion/Float to Integer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(){
+    
+    float value = 9.9;
+    
+    // method 1
+    int valueConverted = (int)value;
+    printf("%d", valueConverted);
+    
+    
+    // method 2
+    int valueConverted1 = value;
+    printf("%d", valueConverted1);
+    
+}
+
---
+++ Type Conversion/Integer to Float.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(){
+    
+    int num = 999;
+    
+    // method 1
+    float numConverted = (1.0) * num;
+    printf("%f", numConverted);
+    
+    
+    // method 2 
+    float numConverted1 = num;
+    printf("%f", numConverted1);
+    
+}
